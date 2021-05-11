d5eece52c699aa28fba4c5db74d066f131f0abd8
anddinara <and.dinara.moscow@gmail.com>
2021-05-11 22:36:33 +0300

    add random function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-import './popup.js';
-import './api.js';
-import './map.js';
-import './user-form.js';
+const getRandomNumber = (min, max, floating = 0) => {
+  if (min < 0 || max < 0) {
+    return 0;
+  } else if (max < min) {
+    return min;
+  } else {
+    const randomNumber = _.random(min, max, true);
+    return randomNumber.toFixed(floating);
+  }
+};
